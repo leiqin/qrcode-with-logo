3d3db761ea8b59c0649ae520bad1b354ef0ab8cb
leiqin <leiqin2010@gmail.com>
2014-03-18 23:39:47 +0800

    + 添加一个 bash 脚本

--- src/java/name/leiqin/qrcode/Test.java
+++ src/java/name/leiqin/qrcode/Test.java
@@ -121,7 +121,8 @@ public class Test extends JComponent {
 			context = args[2];
 		} else if (args.length == 4) {
 			output = args[0];
-			size = Integer.valueOf(args[1]);
+			if (args[1].trim().length() != 0)
+				size = Integer.valueOf(args[1]);
 			logopath = args[2];
 			context = args[3];
 		}
